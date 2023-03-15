4f499ab9516494000bcf044d48a17f143d1a33c8
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2023-03-15 17:08:11 +0000

    1 file modified, 5 added

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { SharedModule } from './shared.module';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [SharedModule],
   template: `
     <h1>Publications!</h1>
+
+    <app-posts></app-posts>
   `,
   styles: [
     `
---
+++ src/posts/posts.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared.module';
+import { PostsComponent } from './posts.component';
+
+@NgModule({
+  imports: [SharedModule],
+  declarations: [PostsComponent],
+  exports: [PostsComponent],
+})
+export class PostsModule {}
